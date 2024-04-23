6d0d5b705bab20fb1a25f28ae8da3dd7c368fbc3
Taylor Sims <trsims@mail.bradley.edu>
2024-04-23 14:55:07 -0500

    Revert "map done"
    This reverts commit 3684c77f5d08c072a51be760ec26c2abff6eb7fa.

--- app/src/main/java/com/example/robotapp/RobotDatabase.java
+++ app/src/main/java/com/example/robotapp/RobotDatabase.java
@@ -1,45 +1,24 @@
 package com.example.robotapp;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class RobotDatabase extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    FloatingActionButton add_button;
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_insert_data);
+        setContentView(R.layout.activity_robot_database);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        recyclerView = findViewById(R.id.recyclerView);
-        add_button = findViewById(R.id.add_button);
-        add_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddActivity.class);
-                startActivity(intent);
-            }
-        });
     }
-
 }
